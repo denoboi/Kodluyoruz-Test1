b6640044cde3802628f9f84ac71538d849fe9144
denoboi <deniz.kaymakci@hotmail.com>
2021-08-16 00:52:03 +0300

    Player Move

--- Assets/[Game]/Scripts/Player.cs
+++ Assets/[Game]/Scripts/Player.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody rigidbody;
-    Rigidbody Rigidbody { get { return (rigidbody == null)}? rigidbody = GetComponent<Rigidbody>(): rigidbody; }}
+    private Rigidbody rb;
+    public Rigidbody Rigidbody // { get { return (rigidbody == null) ? rigidbody = GetComponent<Rigidbody>(): rigidbody; } }
+
+    {
+        get
+        {
+            if (rb == null)
+            {
+                rb = GetComponent<Rigidbody>();
+            }
+
+            return rb;
+
+        }
+    }
+
+    public float moveSpeed;
+    private void FixedUpdate()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //Rigidbody.AddForce(input * MoveSpeed * Time.deltaTime);
+
+        Rigidbody.velocity = input * moveSpeed * Time.fixedDeltaTime;
+
+    }
 }
